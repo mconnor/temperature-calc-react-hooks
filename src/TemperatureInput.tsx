f2bc0d43f1d76a2e2f2ac8a2f0a8697fb6ba0064
mike connor <mike@rocketnumber9.org>
2020-01-30 12:52:53 -0600

    ts fixes

--- src/TemperatureInput.tsx
+++ src/TemperatureInput.tsx
@@ -2,31 +2,32 @@ import React from 'react';
 
 type MyDumbProps = {
     onTemperatureChange: (t: number) => void;
-    scale?: string;
+    scale: string;
     temperature?: number;
-  };
+};
 
 
-  type InputEvent = React.ChangeEvent<HTMLInputElement>;
-// const scaleNames = {
-// 	c: 'Celsius',
-// 	f: 'Fahrenheit'
-// };
+type InputEvent = React.ChangeEvent<HTMLInputElement>;
 
 
-const TemperatureInput = ({temperature, onTemperatureChange, scale}:MyDumbProps) => {
-    //const [temp, setTemp] = useState<number>()
+const scaleNames =  {
+    'c': 'Celsius',
+    'f': 'Fahrenheit'
+}
+
+const TemperatureInput = ({ temperature, onTemperatureChange, scale }: MyDumbProps) => {
+    let _scale: string = (scale === 'c' ? scaleNames[scale] : scaleNames[scale]);
     return (
         <div>
             <fieldset>
-				<legend>Enter temperature in {scale}:</legend>
-                <input 
-                    value={temperature} 
-                    onChange={(e) => onTemperatureChange(parseInt(e.target.value))} 
+                <legend>Enter temperature in {_scale}:</legend>
+                <input
+                    value={temperature}
+                    onChange={(e) => onTemperatureChange(parseInt(e.target.value))}
                     type="number" />
-			</fieldset> 
+            </fieldset>
         </div>
     )
 }
 
-export default  TemperatureInput
+export default TemperatureInput
